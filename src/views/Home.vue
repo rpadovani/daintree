45dc5de3fcc607ea86fc2be76c420b463593272e
Riccardo Padovani <rpadovani@daintree.app>
2020-07-05 14:33:32 +0200

    New homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,88 @@
 <template>
-  <section class="gl-banner">
-    <img
-      src="/assets/undraw_japan_ubgk.svg"
-      alt="Daintree"
-      role="presentation"
-      class="col-5 d-none d-md-block"
-    />
-    <div class="gl-banner-content">
-      <h1>Welcome to Daintree!</h1>
-      <h4>
-        An opensource alternative implementation of the Amazon Web Services'
-        console.
-      </h4>
-      <p class="mt-2">
-        Daintree is a website to manage some of your AWS resources: since this
-        is an early preview, at the moment, it supports a subset of
-        <b>Networking</b>, <b>EC2</b>, <b>SQS</b>, and <b>SNS</b>. We are
-        working on extending support to other resources!
-      </p>
-      <p>
-        Daintree does not aim to replace the original console, but would like to
-        improve the user experience offering:
-      </p>
-      <ul>
-        <li>
-          <b>Multi-region view</b>: why should you observe resources from only
-          one region, when there are 25 available?
-        </li>
-        <li>
-          <b>Unified UX</b>: all components follow the same style, to have a
-          coherent experience;
-        </li>
-        <li>
-          <b>Fast navigation</b>: Daintree aims to be fast, without full page
-          reloads, and making switching between roles, regions, and services
-          painless;
-        </li>
-        <li>
-          <b>...and much more:</b> Daintree tries to improve the daily
-          experience of using AWS services from the web! You can read about all
-          our features in the dedicated page, or login and try it!
-        </li>
-      </ul>
+  <div class="container">
+    <div class="row mt-5 text-center">
+      <gl-empty-state
+        compact
+        title="Welcome to Daintree.app"
+        svg-path="/assets/undraw_japan_ubgk.svg"
+        description="An opensource alternative implementation of the Amazon Web Services' console, with multi-region support!"
+        primary-button-link="/#/login"
+        primary-button-text="Login"
+        secondary-button-link="/#/about"
+        secondary-button-text="See all features"
+      />
+    </div>
 
-      <div class="row justify-content-center">
-        <gl-button-group>
-          <gl-button variant="info" category="secondary" to="/about"
-            >See all features
-          </gl-button>
-          <gl-button category="secondary" to="/security"
-            >What about security?</gl-button
-          >
-          <gl-button to="/login" variant="success">Login</gl-button>
-        </gl-button-group>
+    <div class="row mt-5 text-center justify-content-between">
+      <div class="col-12 col-md-4">
+        <gl-card class="col-11 pl-0 pr-0">
+          <template #header>
+            <h4>Multi region support</h4>
+          </template>
+          <img
+            class="col-12"
+            src="/assets/undraw_connected_world_wuay.svg"
+            alt="Regions selector"
+          />
+          <p class="mt-1">
+            Why should you observe resources from only one region, when there
+            are 25 available?
+          </p>
+        </gl-card>
       </div>
 
-      <p class="mt-3">
+      <div class="col-12 col-md-4">
+        <gl-card class="col-11 pl-0 pr-0">
+          <template #header>
+            <h4>Secure</h4>
+          </template>
+          <img
+            class="col-12 mt-2 mb-2"
+            src="/assets/undraw_privacy_protection_nlwy.svg"
+            alt="Privacy protection"
+          />
+          <p class="mt-1">
+            No ads, no spyware, no trackers: all communications stay between you
+            and AWS.
+          </p>
+        </gl-card>
+      </div>
+
+      <div class="col-12 col-md-4">
+        <gl-card class="col-11 pl-0 pr-0">
+          <template #header>
+            <h4>Coherent interface</h4>
+          </template>
+          <img
+            class="col-12 mt-2 mb-2"
+            src="/assets/undraw_redesign_feedback_48ti.svg"
+            alt="Cool design"
+          />
+          <p class="mt-1">
+            All pages follow the same style, to have a pleasant experience.
+          </p>
+        </gl-card>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-12">
+        <h4>...and much, much more!</h4>
+        <p>
+          Help texts to navigate easily among the thousands feature of AWS, fast
+          role switching to manage multiple accounts, instantaneous navigation
+          to change page without waiting, beautiful graphs you can resize and
+          download to monitor performances, and much more: you can take a look
+          to all the features in
+          <router-link to="/about">the dedicated page</router-link>.
+        </p>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <p class="small">
         This website is not affiliated to Amazon, or AWS, or any of their
-        subsidiaries. All trademarks registered. Source code on
+        subsidiaries. Source code on
         <a href="https://gitlab.com/rpadovani/daintree" target="_blank"
           >Gitlab
           <gl-icon name="external-link" />
@@ -77,17 +102,19 @@
         Daintree takes
         <router-link to="/security"
           >your privacy and security seriously</router-link
-        >: no ads, no trackers, no data stored, no external resources.
+        >: no ads, no trackers, no data stored, no external resources. You can
+        <router-link to="/contribute">contribute</router-link> too!
       </p>
     </div>
-  </section>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Header from "@/components/Header/Header.vue";
-import { GlButton, GlButtonGroup, GlIcon } from "@gitlab/ui";
+import { GlIcon, GlEmptyState, GlCard } from "@gitlab/ui";
 
-@Component({ components: { Header, GlButton, GlButtonGroup, GlIcon } })
+@Component({
+  components: { GlCard, GlEmptyState, GlIcon },
+})
 export default class Home extends Vue {}
 </script>

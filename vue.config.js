244722a7e76c0ee30dfde4bd71626596810d1fa5
Riccardo Padovani <gitlab@rpadovani.com>
2020-07-07 19:32:13 +0000

    Prerender the homepage

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 process.env.VUE_APP_VERSION = require("./package.json").version;
 
 module.exports = {
   transpileDependencies: ["@gitlab/ui"],
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, "dist"),
+        // Required - Routes to render.
+        routes: ["/"],
+      }),
+    ],
+  },
 };
